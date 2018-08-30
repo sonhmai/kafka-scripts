882f3e0cfb07608348e0d49dac6929123d9f0e85
Son Mai <hongson1104@gmail.com>
2018-08-30 13:56:19 +0700

    .

--- list-all-topics-raw.sh
+++ list-all-topics-raw.sh
@@ -5,10 +5,5 @@ BROKERPORT=9092
 ZKPORT=2181
 CDIR=$(pwd)
 cd /opt/kafka_2.11-0.11.0.0
-echo "-----LIST ALL TOPICS IN CLUSTER VIA ZK------"
-echo 
 bin/kafka-topics.sh --zookeeper $kafka02:$ZKPORT --list
-
-echo "----------non Zookeeper-based consumer groups(Java Consumer API)----------"
-echo 
 bin/kafka-consumer-groups.sh --bootstrap-server $kafka02:$BROKERPORT --list
