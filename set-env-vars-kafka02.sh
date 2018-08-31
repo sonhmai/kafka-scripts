bc9ceab2c1bc383dd1a11d0937a8b867ff8fc812
Son Mai <hongson1104@gmail.com>
2018-08-31 10:35:52 +0700

    .

--- set-env-vars-kafka02.sh
+++ set-env-vars-kafka02.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export KAFKA_HOME = '/opt/kafka_2.11-1.1.1/'
-export ZOOKEEPER_SOCKET = '183.80.199.5:2181'
-export KAFKA_CONSUMER_GROUP = 'prod_spark_streaming'
+export KAFKA_HOME '/opt/kafka_2.11-1.1.1/'
+export ZOOKEEPER_SOCKET '183.80.199.5:2181'
+export KAFKA_CONSUMER_GROUP 'prod_spark_streaming'
 
 echo 'set KAFKA_HOME to $KAFKA_HOME'
 echo 'set ZOOKEEPER_SOCKET to $ZOOKEEPER_SOCKET'
